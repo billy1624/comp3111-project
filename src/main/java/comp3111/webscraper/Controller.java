37eb7d1c2ea13ebc9a55a6002b3952283268ca13
Billy Chan <ccw.billy.123@gmail.com>
2018-09-14 13:52:05 +0800

    init

---
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package comp3111.webscraper;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Hyperlink;
+import java.util.List;
+
+
+/**
+ * 
+ * @author kevinw
+ *
+ *
+ * Controller class that manage GUI interaction. Please see document about JavaFX for details.
+ * 
+ */
+public class Controller {
+
+    @FXML 
+    private Label labelCount; 
+
+    @FXML 
+    private Label labelPrice; 
+
+    @FXML 
+    private Hyperlink labelMin; 
+
+    @FXML 
+    private Hyperlink labelLatest; 
+
+    @FXML
+    private TextField textFieldKeyword;
+    
+    @FXML
+    private TextArea textAreaConsole;
+    
+    private WebScraper scraper;
+    
+    /**
+     * Default controller
+     */
+    public Controller() {
+    	scraper = new WebScraper();
+    }
+
+    /**
+     * Default initializer. It is empty.
+     */
+    @FXML
+    private void initialize() {
+    	
+    }
+    
+    /**
+     * Called when the search button is pressed.
+     */
+    @FXML
+    private void actionSearch() {
+    	System.out.println("actionSearch: " + textFieldKeyword.getText());
+    	List<Item> result = scraper.scrape(textFieldKeyword.getText());
+    	String output = "";
+    	for (Item item : result) {
+    		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
+    	}
+    	textAreaConsole.setText(output);
+    	
+    	
+    }
+    
+    /**
+     * Called when the new button is pressed. Very dummy action - print something in the command prompt.
+     */
+    @FXML
+    private void actionNew() {
+    	System.out.println("actionNew");
+    }
+}
+
---
+++ src/main/java/comp3111/webscraper/Item.java
@@ -0,0 +1,30 @@
+package comp3111.webscraper;
+
+
+
+public class Item {
+	private String title ; 
+	private double price ;
+	private String url ;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+
+}
